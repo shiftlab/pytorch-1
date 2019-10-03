cff9bc0b527aaa6d453cffdb25d1bde35bcbde68
David Reiss <dreiss@fb.com>
2019-10-03 16:36:50 -0700

    JavaDoc

---
+++ android/pytorch_android/doclava-out/docs/assets/navtree_data.js
@@ -0,0 +1,7 @@
+var NAVTREE_DATA =
+[ [ "org.pytorch", "org/pytorch/package-summary.html", [ [ "Classes", null, [ [ "IValue", "org/pytorch/IValue.html", null, "" ], [ "Module", "org/pytorch/Module.html", null, "" ], [ "Tensor", "org/pytorch/Tensor.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
---
+++ android/pytorch_android/doclava-out/docs/reference/lists.js
@@ -0,0 +1,7 @@
+var DATA = [
+      { id:0, label:"org.pytorch", link:"org/pytorch/package-summary.html", type:"package" },
+      { id:1, label:"org.pytorch.IValue", link:"org/pytorch/IValue.html", type:"class" },
+      { id:2, label:"org.pytorch.Module", link:"org/pytorch/Module.html", type:"class" },
+      { id:3, label:"org.pytorch.Tensor", link:"org/pytorch/Tensor.html", type:"class" }
+
+    ];
